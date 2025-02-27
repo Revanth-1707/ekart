57646d1d98427f633a76adc8977fa4050510bbd4
Revanth-1707 <revanthagastya373@gmail.com>
2025-02-27 12:05:09 +0530

    Ekart 1st commit

--- src/main/java/org/jsp/ekart/EkartApplication.java
+++ src/main/java/org/jsp/ekart/EkartApplication.java
@@ -2,12 +2,12 @@ package org.jsp.ekart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EkartApplication {
+public class EkartApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EkartApplication.class, args);
 	}
-
 }
